88cc9f748f13196e9a8878d8486a9c1f6667c490
Nerusupalli Meher Freiney <83865700+Freineymehera@users.noreply.github.com>
2022-10-10 13:03:33 +0530

    n=int(input()) fa=0 fb=1 fn=fa+fb c=0 for i in range(100):     if fa==n:         c=1         break     else:         fn=fa+fb         fa=fb         fb=fn         continue if c==1:     print("True") else:     print("False")

---
+++ Fibonacci_or_not.py
@@ -0,0 +1,18 @@
+n=int(input())
+fa=0
+fb=1
+fn=fa+fb
+c=0
+for i in range(100):
+    if fa==n:
+        c=1
+        break
+    else:
+        fn=fa+fb
+        fa=fb
+        fb=fn
+        continue
+if c==1:
+    print("True")
+else:
+    print("False")
