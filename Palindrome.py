403a85e515d553cd02c3df94fc580cb035673ae4
Nerusupalli Meher Freiney <83865700+Freineymehera@users.noreply.github.com>
2022-10-11 21:11:42 +0530

    def rev(n):     s=0     while n>0:         rem=n%10         s=(s*10)+rem         n=n//10     return s a=int(input()) if(a==rev(a)):     print("True") else:     print("False")

--- Palindrome.py
+++ Palindrome.py
@@ -1,6 +1,13 @@
-n=str(input())
-reverse=n[::-1]
-if reverse==n:
+def rev(n):
+    s=0
+    while n>0:
+        rem=n%10
+        s=(s*10)+rem
+        n=n//10
+    return s
+a=int(input())  
+if(a==rev(a)):
     print("True")
 else:
     print("False")
+    
