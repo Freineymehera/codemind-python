b6c99d1976a66652dfe03ead272d4248ddbb6672
Nerusupalli Meher Freiney <83865700+Freineymehera@users.noreply.github.com>
2022-10-16 16:53:35 +0530

    n=int(input()) arr=list(map(int,input().split())) k=int(input()) c=0 for i in range(len(arr)):     for j in range(2,arr[i]):         if arr[i]%j==0:             break     else:         if arr[i]!=1 and arr[i]<=k:             c+=1 print(c)

---
+++ primes_less_than_k.py
@@ -0,0 +1,12 @@
+n=int(input())
+arr=list(map(int,input().split()))
+k=int(input())
+c=0
+for i in range(len(arr)):
+    for j in range(2,arr[i]):
+        if arr[i]%j==0:
+            break
+    else:
+        if arr[i]!=1 and arr[i]<=k:
+            c+=1
+print(c)
